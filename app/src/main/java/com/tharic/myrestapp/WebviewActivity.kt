d6d77f4ce479cb7674ac33b8ae24d39e89177b31
thomas7917 <r.monaccuia67@gmail.com>
2025-06-03 17:45:16 +0200

    Aggiornamento WebviewActivity.kt

--- app/src/main/java/com/tharic/myrestapp/WebviewActivity.kt
+++ app/src/main/java/com/tharic/myrestapp/WebviewActivity.kt
@@ -1,20 +1,25 @@
-package com.tharic.myrestapp
+package com.example.myrestapp
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class WebviewActivity : AppCompatActivity() {
+class WebViewActivity : AppCompatActivity() {
+    private val TAG = "WebviewActivity - MyRESTApp"
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_webview)
+        setContentView(R.layout.activity_web_view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        webView = findViewById<WebView>(R.id.WebView)
+        webView.loadUrl("https://www.netflix.com/it/browse/genre/34399")
     }
 }
