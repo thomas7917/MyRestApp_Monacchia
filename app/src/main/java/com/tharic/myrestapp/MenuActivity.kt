76f7ee09ac909fbbefa9f18f100cbc4f51753eb2
thomas7917 <r.monaccuia67@gmail.com>
2025-06-03 17:42:20 +0200

    Aggiornamento MenuActivity.kt

--- app/src/main/java/com/tharic/myrestapp/MenuActivity.kt
+++ app/src/main/java/com/tharic/myrestapp/MenuActivity.kt
@@ -1,6 +1,11 @@
 package com.tharic.myrestapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,19 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun onClickMenu(){
+        var btnMenu = findViewById<ImageButton>(R.id.ButtonNetflix)
+        btnMenu.setOnClickListener(View.OnClickListener {
+            messaggioToastMenu("LOGIN EFFETTUATO!")
+        })
+    }
+    fun messaggioToastMenu(msg: String){
+        var mioToast = Toast.makeText(
+            this,
+            msg,
+            Toast.LENGTH_SHORT
+        )
+        mioToast.show()
     }
 }
