d2869824ef4946421324a3a7e169c0859b36d85d
thomas7917 <r.monaccuia67@gmail.com>
2025-06-05 09:09:04 +0200

    Login creato e password criptata

--- app/src/main/java/com/tharic/myrestapp/LoginActivity.kt
+++ app/src/main/java/com/tharic/myrestapp/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.tharic.myrestapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.security.MessageDigest
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,31 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+        val nome = findViewById<EditText>(R.id.nome)
+        val pwd = findViewById<EditText>(R.id.password)
+        val login = findViewById<Button>(R.id.login)
 
+        //credenziali
+        val username = "thomas"
+        val password = "e8ff748e5a9347456fd452177fa5f31cb542c4c469910569da01126c93d35cea"
+
+        login.setOnClickListener {
+            val nomeS = nome.text.toString()
+            val passwordC = sha256(pwd.text.toString())
+
+            if(username==nomeS && password==passwordC){
+                val mioToast = Toast.makeText(this, "Login avvenuto!", Toast.LENGTH_LONG)
+                mioToast.show()
+            }
+            else{
+                val mioToast = Toast.makeText(this, "Accesso negato!", Toast.LENGTH_LONG)
+                mioToast.show()
+            }
+        }
+    }
+
+    private fun sha256(input: String): String {
+        val bytes = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
+        return bytes.joinToString("") { "%02x".format(it) }
     }
 }
