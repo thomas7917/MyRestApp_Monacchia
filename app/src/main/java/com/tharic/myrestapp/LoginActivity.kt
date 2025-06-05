18c5079a13a460ddcacc82e81e1d4dce585d7ef5
thomas7917 <r.monaccuia67@gmail.com>
2025-06-05 08:41:21 +0200

    risolti errori

--- app/src/main/java/com/tharic/myrestapp/LoginActivity.kt
+++ app/src/main/java/com/tharic/myrestapp/LoginActivity.kt
@@ -1,10 +1,6 @@
-package com.example.myrestapp
+package com.tharic.myrestapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,28 +11,6 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-    fun onClick(){
-        var btnClick = findViewById<Button>(R.id.button)
-        btnClick.setOnClickListener(View.OnClickListener {
-            messaggioToast("LOGIN EFFETTUATO!")
-        })
-    }
-    private fun creaIntent(){
-        var Intent= Intent(this,WebViewActivity::class.java)
-        startActivity(Intent)
-    }
-    private fun messaggioToast(msg: String) {
-        var mioToast = Toast.makeText(
-            this,
-            msg,
-            Toast.LENGTH_SHORT
-        )
-        mioToast.show()
+
     }
 }
--- app/src/main/java/com/tharic/myrestapp/MenuActivity.kt
+++ app/src/main/java/com/tharic/myrestapp/MenuActivity.kt
@@ -1,11 +1,6 @@
 package com.tharic.myrestapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,24 +11,6 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-    fun onClickMenu(){
-        var btnMenu = findViewById<ImageButton>(R.id.ButtonNetflix)
-        btnMenu.setOnClickListener(View.OnClickListener {
-            messaggioToastMenu("LOGIN EFFETTUATO!")
-        })
-    }
-    fun messaggioToastMenu(msg: String){
-        var mioToast = Toast.makeText(
-            this,
-            msg,
-            Toast.LENGTH_SHORT
-        )
-        mioToast.show()
+
     }
 }
